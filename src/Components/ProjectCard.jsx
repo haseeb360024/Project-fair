2028fd456a5c88d767b92561c5abad129eea60b8
haseeb360024 <muhammedhaseeb315@gmail.com>
2023-10-26 00:47:06 -0700

    last

---
+++ src/Components/ProjectCard.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { Card, Col, Modal, Row } from 'react-bootstrap'
+import projectPic from '../Assets/pr1.png'
+
+function ProjectCard() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  return (
+    <>
+    <Card className='shadow mb-5 btn' onClick={handleShow}>
+      <Card.Img variant="top" src={projectPic} />
+      <Card.Body>
+        <Card.Title>Project Title</Card.Title>
+      </Card.Body>
+    </Card>
+
+<Modal size='lg' show={show} onHide={handleClose}>
+
+    <Modal.Header closeButton>
+        <Modal.Title>Modal Heading</Modal.Title>
+    </Modal.Header>
+    <Modal.Body> 
+     <Row>
+        <Col>
+            <img style={{height:'200px'}} className='img-fluid' src={projectPic} alt="project image" />
+        </Col>
+        <Col md={6}>
+            <h2>Project Title</h2>
+            <p>sdfsdjsdsd  dgvsdfgbs sdhgbsdgvvbsdfg sdfvbds ghsdnafgv vdf sdhfg ufbwefbhs fsdfhfb sdsyhv dfvbubfg sd ghs vsd vsdvsufgs nsd vujsdvg n vhdsvbds vsdjhvdbsvsdbn ms bvuiosdvb vjsdivsivsdkvbisv sdjvbnsigvsdmvsd hn</p>
+            <p>Language Used: <span className='fw-bolder'> HTML, CSS ,React</span></p>
+
+
+        </Col>
+        </Row>   
+
+        <div className='mt-3'>
+            <a href="https://github.com/haseeb360024/Project-fair" target='_blank' className='me-3 btn'> <i className='fa-brands fa-github fa-2x'></i></a>
+            <a href="https://e-kart-brown.vercel.app/" target='_blank' className='me-5 btn'> <i className='fa-solid fa-link fa-2x'></i></a>
+        </div>
+    </Modal.Body>
+</Modal>
+
+    </>
+  )
+}
+
+export default ProjectCard
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
-
+import { Col, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import ProjectCard from '../Components/ProjectCard'
+import titleImage from '../Assets/download.png'
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    {/*landing section */}
+    <div style={{width:'100%', height:'100vh',backgroundColor:'green'}}
+     className='container-fluid rounded'>
+      <Row className='align-items-center p-5'>
+        <Col sm={12} md={6}>
+          <h1 style={{fontSize:'80px'}} className='fw-bolder text-light'> <i class="fa-brands fa-stack-overflow fa-bounce"></i> Project Fair</h1>
+          <p>One Stop Destination for all software hgdfjgdifgdg dfgjdfhg dfgjdfgn dgbujdfbgdf mguidfg dfgbjhdjg dfbnhdfg fdbudfhg dfnmmbdfkbdfb inbdf bidfbdf bfdibidfbnfdibnfidgbobdf bdfoghdfnbuidfb dfb odfbdfkbdfkb dfbfdibfd buidb dfbdfuibrfbfdgb </p>
+          <button className=' btn btn-warning'>Start to Explore <i class="fa-solid fa-right-long fa-beat ms-2"></i></button>
+        </Col>
+
+        <Col sm={12} md={6}>
+          <img style={{marginTop:'100px'}} className='w-75' src={titleImage} alt='' />
+          
+        </Col>
+      </Row>
+      
+       </div>
+
+       {/*all projects */}
+       <div className="all-projects mt-5">
+        <h1 className='text-center'> Explore Our Projects</h1>
+
+        <marquee scrollAmount={25}>
+        <div className='d-flex justify-content-between'>
+
+          <div style={{width:'500px'}}>
+            <ProjectCard/>
+            </div>
+      
+            </div>
+        </marquee>
+        <div className="text-center mt-5"> <Link to={'/projects'}>View More Projects</Link></div>
+       </div>
+       
+    
+    </>
   )
 }
 
